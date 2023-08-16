acd0d875f4567e038079c69154ce7d535b84f01b
zhangkai <zhangkai@smart-insight.com.cn>
2023-08-16 10:02:53 +0800

    c编译器配置

---
+++ c/helloworld.c
@@ -0,0 +1,14 @@
+/**
+ * file: helloworld.c
+ * author: 简单教程(www.twle.cn)
+ */
+// 预处理器指令, 编译之前加载头文件
+#include <stdio.h>
+int main()
+{
+   /* C 语言世界第一波 */
+   printf("Hello, World! \n");
+   printf("Hello 简单教程!\n");
+
+   return 0;
+}
---
+++ 流畅的python/c/helloworld.c
@@ -0,0 +1,15 @@
+/**
+ * file: helloworld.c
+ * author: 简单教程(www.twle.cn)
+ */
+
+#include <stdio.h>
+
+int main()
+{
+   /* C 语言世界第一波 */
+   printf("Hello, World! \n");
+   printf("Hello 简单教程!\n");
+
+   return 0;
+}
